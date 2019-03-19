90cd6a4a194ac24db2dc78b62684e5a617259486
julian_rico18 <julian.rico@correounivalle.edu.co>
2019-03-19 15:20:38 -0500

    Commit desde el CIER(Todos los proyectos completos), hasta este punto he llegado al tema de routing/music pagina 410 del libro

---
+++ testing/basics/Modash.test.js
@@ -0,0 +1,13 @@
+describe('My test suite', () => {
+  it('`true` should be `true`', () => {
+    expect(true).toBe(true);
+  });
+
+  it('`false` should be `false`', () => {
+    expect(false).toBe(false);
+  });
+});
+
+
+// We write the tests for the Modash library in
+// this file in the Unit Testing chapter
